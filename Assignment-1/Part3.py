c9dffaf3dcf800be665c3852c7d5917833bc20bc
Ana Laura Chioca Vieira <48166530+anachioca@users.noreply.github.com>
2021-04-25 12:01:16 -0300

    Queue finished

--- Assignment-1/Part3.py
+++ Assignment-1/Part3.py
@@ -25,8 +25,44 @@ class Stack:
 
     def size(self):
         return len(self.stack)
+    
+    
+class Queue:
 
-def test():
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, n):
+        self.queue.append(n)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            dequeued = self.queue[0]
+            self.queue = self.queue[1:]
+            return dequeued
+
+    def rear(self):
+        if len(self.queue) == 0:
+            print("Queue is empty.")
+            return False
+        return self.queue[len(self.queue)-1]
+
+    def front(self):
+        if len(self.queue) == 0:
+            print("Queue is empty.")
+            return False
+        return self.queue[0]
+
+    def size(self):
+        return len(self.queue)
+
+    def isEmpty(self):
+        if len(self.queue) == 0:
+            return True
+        return False
+
+
+def testStack():
     myStack = Stack()
     myStack.push(42)
     print("Top of stack: ", myStack.top())
@@ -42,4 +78,21 @@ def test():
     print("Popped value: " , popped_value)
     # prints "Stack is empty. No element to pop. Popped value:  None"
 
-test()
+def testQueue():
+    myQueue = Queue()
+    myQueue.enqueue(1)
+    myQueue.enqueue(2)
+    myQueue.enqueue(3)
+    print("Size of queue: ", myQueue.size())
+    # prints “Size of queue: 3”
+    print("Front of queue: ", myQueue.front())
+    # prints “Front of queue: 1”
+    print("Rear of queue: ", myQueue.rear())
+    # prints “Rear of queue: 3”
+    dequeuedItem = myQueue.dequeue()
+    print("Dequeue item: ", dequeuedItem)
+    # prints “Dequeued item: 1”
+
+
+testStack()
+testQueue()
